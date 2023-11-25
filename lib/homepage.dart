560243e95775445169f749d776125f20d5108768
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-11-25 08:49:47 +0530

    adding a divider

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -18,6 +18,12 @@ class HomePage extends StatelessWidget {
           SizedBox(height: 10,),
           const IconAndDetail(Icons.calendar_today,'November 25'),
           const IconAndDetail(Icons.location_city,' Bangalore'),
+          const Divider(
+            height: 8,
+            thickness: 1,
+            indent: 8,
+            color: Colors.grey,
+          ),
           const Header("What we'll be doing"),
           const Paragraph('join us for a day full of Firebase Workshops!')
         ],
@@ -25,3 +31,5 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+
--- lib/src/widgets.dart
+++ lib/src/widgets.dart
@@ -50,3 +50,18 @@ class Paragraph extends StatelessWidget {
         ),
       );
 }
+
+
+class StyledButton extends StatelessWidget {
+  const StyledButton({required this.child, required this.onPressed, super.key});
+  final Widget child;
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) => OutlinedButton(
+        style: OutlinedButton.styleFrom(
+            side: const BorderSide(color: Colors.deepPurple)),
+        onPressed: onPressed,
+        child: child,
+      );
+}
