d277e5354736afa08f5981fc75a14be29b4b4c01
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-11-26 17:49:52 +0530

    adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meetup_app/homepage.dart';
-import 'package:meetup_app/firebase_options.dart';
 
 void main() {
   runApp(MyApp());
